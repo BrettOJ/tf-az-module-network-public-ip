dec5b8041ac71a9bb15491c619f9f6f90921ff9c
Brett OdgersJewell <Brett.OdgersJewell@insearch.edu.au>
2023-10-31 09:14:24 +0800

    update module

--- module.tf
+++ module.tf
@@ -4,6 +4,11 @@ resource "azurerm_public_ip" "pip_object" {
   resource_group_name = var.resource_group_name
   sku                 = var.sku
   allocation_method   = lower(var.sku) == "standard" ? "Static" : var.allocation_method
+  ddos_protection_mode    = var.ddos_protection_mode
+  ddos_protection_plan_id = var.ddos_protection_plan_id
+  edge_zone               = var.edge_zone
+  ip_tags                 = var.ip_tags
+  ip_version              = var.ip_version
   domain_name_label   = var.domain_name_label
   zones               = var.zones
   reverse_fqdn        = var.fqdn
